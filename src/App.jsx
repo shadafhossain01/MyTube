62f41a7f02b5a34f4a97fe2a66595a56da42997a
Shadaf Hossain <shadafhossain01@gmail.com>
2026-02-06 00:32:48 +0600

    Router Setup Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-function App() {
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+import WatchPage from "./pages/WatchPage";
+import MainLayout from "./pages/MainLayout";
 
+function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/watch" element={<WatchPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App
---
+++ src/pages/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return <div>
+  MainLayout
+  <Outlet/>
+  </div>;
+};
+
+export default MainLayout;
